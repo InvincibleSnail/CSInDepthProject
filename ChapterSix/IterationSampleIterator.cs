f1fa2f22f5588b0f3a10b866ac05911798c4f44d
shenhua <695675124@qq.com>
2024-05-17 00:06:46 +0800

    chapter 6;

---
+++ ChapterSix/IterationSampleIterator.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+
+namespace CSInDepthProject.ChapterSix;
+
+public class IterationSampleIterator : IEnumerator
+{
+    private IterationSample parent;
+    private int position;
+
+    internal IterationSampleIterator(IterationSample parent)
+    {
+        this.parent = parent;
+        position = -1;
+    }
+
+    public bool MoveNext()
+    {
+        if (position != parent.values.Length)
+        {
+            position++;
+        }
+
+        return position < parent.values.Length;
+    }
+
+    public void Reset()
+    {
+        position = -1;
+    }
+
+    public object Current
+    {
+        get
+        {
+            if (position == -1 || position == parent.values.Length)
+            {
+                throw new InvalidOperationException();
+            }
+
+            int index = position + parent.startingPoint;
+            index = index % parent.values.Length;
+            return parent.values[index];
+        }
+    }
+}
