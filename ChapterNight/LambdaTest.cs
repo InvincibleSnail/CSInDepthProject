d5b38a7b5fe2fd5bf8bcef5e47f45ca0bd340cb8
shenhua <695675124@qq.com>
2024-05-21 22:14:04 +0800

    chapter 9;

---
+++ ChapterNight/LambdaTest.cs
@@ -0,0 +1,14 @@
+﻿namespace CSInDepthProject.ChapterNight;
+
+public class LambdaTest
+{
+    private Func<string, int> returnLength;
+
+    public LambdaTest()
+    {
+        returnLength = delegate(string text) { return text.Length; };
+        Console.Write(returnLength("hello"));
+        returnLength = s => s.Length;
+        Console.Write(returnLength("ooo"));
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,14 +1,17 @@
-﻿using System.Text;
-using CSInDepthProject.ChapterEight;
+﻿using System.Linq.Expressions;
+using System.Text;
+using CSInDepthProject.ChapterNight;
 
 namespace CSInDepthProject;
 
 class Programw
 {
+    static void PrintTValue<T>(T t)
+    {
+        Console.WriteLine(t);
+    }
     static void Main(string[] args)
     {
-        List<int> l = new List<int>() { 1, 2, 3, 4, 5 };
-        var test = new { He = "aha", She = "wtf" };
-        Console.WriteLine(test.He);
+        PrintTValue<ExpressionTest>(new ExpressionTest());
     }
 }
